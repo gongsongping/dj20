e27b88432984e76f0a072040d1f08efb7e608428
gongsongping <gongsongping@gmai.com>
2018-04-26 23:03:01 +0800

    profile-user-serialize

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
 from .models import *
+from django.contrib.auth.models import User
 
 
+class UserSerializer(serializers.ModelSerializer):
+    # ,'tagprofile_profiles','tagpost_posts'
+    class Meta:
+        model = User
+        exclude = ('password',)        
+        # fields = ('id','name','created_at','updated_at')
 
 class TagSerializer(serializers.ModelSerializer):
     # ,'tagprofile_profiles','tagpost_posts'
@@ -23,16 +30,27 @@ class PhotoSerializer(serializers.ModelSerializer):
 
 class PostSerializer(serializers.ModelSerializer):
 
+    comment_count = serializers.SerializerMethodField()
+    def get_comment_count(self, obj):
+        return obj.comments.count()
+
     class Meta:
         model = Post
-        fields = ('id', 'content','url','created_at','updated_at','profile')
+        fields = ('id', 'content','url','created_at','updated_at','profile','comments','comment_count')
 
 class ProfileSerializer(serializers.ModelSerializer):
     # posts = serializers.StringRelatedField(many=True)
     # posts = PostSerializer(many=True, read_only=True)
-    # ,'from_tos'
+    user = UserSerializer() #要去除password,所以用此
+
+    # from_tos = serializers.SerializerMethodField()
+    # def get_from_tos(self, obj):
+    #     # return obj.from_tos #Object of type 'Profile' is not JSON serializable
+    #     return  ProfileSerializer(obj.from_tos)  # ProfileSerializer(profiles, many=True)
+    
     class Meta:
         model = Profile
+        # exclude = ('password_digest','token','json')
         fields = ('id', 'name','email','avatar','created_at','updated_at','nationality','city','age','telenumber','user','posts')
         # tab1( id,name,email,password_digest,token,json,avatar,created_at,updated_at,nationality,city,age,telenumber
         depth = 1
